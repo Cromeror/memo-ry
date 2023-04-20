dd2b89ca1bd7b111ed891a5a860e4a99314b4133
Crross <cristobalromerorossi@gmail.com>
2023-04-20 11:25:28 -0500

    style: format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
 
 function App() {
-  return <p className="text-3xl font-bold underline">
-            This is the first commit
-        </p>
+  return (
+    <p className="text-3xl font-bold underline">This is the first commit</p>
+  )
 }
 
 export default App
